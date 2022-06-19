3fc01c84e5bd0b19a3e786706383c58abc4e20f2
TomoichiEbata <52627402+TomoichiEbata@users.noreply.github.com>
2022-06-19 21:55:01 +0900

    Add files via upload

--- money1/main.go
+++ money1/main.go
@@ -1,6 +1,5 @@
 /*
-	"jfe"を、"jfe","jfe","jfe"に一斉変換すれば、対応可能
-
+	"hitachi"を、"toshiba","jfe"と一斉変換すれば、対応可能
 */
 
 package main
@@ -49,18 +48,18 @@ func add_one_day(date string) string {
 
 func main() {
 
-	jfe_db, err := sql.Open("postgres", "user=postgres password=password host=localhost port=15432 dbname=jfe sslmode=disable")
+	hitachi_db, err := sql.Open("postgres", "user=postgres password=password host=localhost port=15432 dbname=hitachi sslmode=disable")
 	if err != nil {
 		log.Fatal("OpenError: ", err)
 	}
-	defer jfe_db.Close()
+	defer hitachi_db.Close()
 
 	add_day := "2021/6/2"
 
 	cash := 1000000 // 100万円からスタート
 	//potential_asset := 0.0 //潜在的資金
-	has_jfe := 0   // 持っていない:0 持っている:1
-	old_close := 0 //
+	has_hitachi := 0 // 持っていない:0 持っている:1
+	old_close := 0   //
 
 	var date string
 	var close int
@@ -75,7 +74,7 @@ func main() {
 
 		select_string := "SELECT date, close from stock where date = '" + add_day + "'"
 
-		rows, err := jfe_db.Query(select_string)
+		rows, err := hitachi_db.Query(select_string)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -92,14 +91,14 @@ func main() {
 		}
 
 		// 本日の江端の行動(昨日より値上がりしていれば買うし、値下がりしていれば売る、という単純な行動)
-		if old_close < close && has_jfe == 0 {
-			has_jfe = 1               // 買い
+		if old_close < close && has_hitachi == 0 {
+			has_hitachi = 1           // 買い
 			cash -= close * 100       // 現金を出して
 			stock_value = close * 100 // 株を買う
 			total = stock_value + cash
 
-		} else if old_close > close && has_jfe == 1 {
-			has_jfe = 0 // 売り
+		} else if old_close > close && has_hitachi == 1 {
+			has_hitachi = 0 // 売り
 			cash += close * 100
 			stock_value = 0 // 株を売る
 			total = stock_value + cash
